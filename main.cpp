345aff3c6a467576994c1a5d7136d48a24da0538
Wenhan Zhu (Cosmos) <cosmos.zhu@uwaterloo.ca>
2017-11-17 01:44:23 -0500

    Added water fountain based on particle system

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include "WaterFountain.h"
+#include "SceneObject.h"
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::Clock clock;
+    float elapsedTime;
+    WaterFountain wf = WaterFountain(300, 200, 300, 300);
 
     while (window.isOpen())
     {
@@ -14,9 +19,13 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        elapsedTime = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        wf.update(elapsedTime);
+        window.setTitle("FPS: " + std::to_string(1.0f/elapsedTime));
 
         window.clear();
-        window.draw(shape);
+        wf.render(&window);
         window.display();
     }
 
